24949bba9370c65fd138f3758c86ad63ca5f237e
Aparnaa-k <aparnaa.kumar@kalvium.community>
2023-09-01 14:39:00 +0530

    fetched the data using axios

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// Progression 1: create a function and fetch the api using axios
+// Progression 1: create a function and fetch the API using axios
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const blog = document.getElementById('blog');
+
+  function getDate() {
+    axios
+      .get(
+        'https://gnews.io/api/v4/top-headlines?token=d1903927956eedce258c43ae64bb345e&lang=en'
+      )
+      .then((res) => {
+        const articleList = res.data.articles;
+        articleList.forEach((el) => {
+          const newArticle = document.createElement('div');
+          newArticle.setAttribute('class', 'article');
+
+          const title = document.createElement('h3');
+          title.innerText = el.title;
+
+          const img = document.createElement('img');
+          img.setAttribute('src', el.image);
+
+          const desc = document.createElement('p');
+          desc.innerText = el.description;
+
+          newArticle.appendChild(title);
+          newArticle.appendChild(img);
+          newArticle.appendChild(desc);
+
+          blog.appendChild(newArticle);
+        });
+      });
+  }
+
+  getDate();
+});
